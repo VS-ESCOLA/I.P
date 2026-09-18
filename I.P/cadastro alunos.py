4d2cdc986f82c95a9f935e91df46b6c95950358e
VS-ESCOLA <emanuel.romero@escola.pr.gov.br>
2026-09-18 11:22:31 -0300

    Add files via upload

---
+++ I.P/cadastro alunos.py
@@ -0,0 +1,12 @@
+lista_alunos = []
+
+def mostrar_nome(nome):
+    print('Novo aluno cadastrado: ', nome)
+    print(lista_alunos)
+while true:
+    nome_novo = input('Digite o nome: ')
+    lista_alunos.append()
+    mostrar_nome(nome_novo)
+cont = print('Quer continuar? (s/n): ') 
+if cont.lower() = 'n':
+    break
