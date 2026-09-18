48441e226a3a6b2d2fa6b8272b67464b50770db9
VS-ESCOLA <emanuel.romero@escola.pr.gov.br>
2026-09-18 15:10:44 -0300

    melhora

--- I.P/cadastro alunos.py
+++ I.P/cadastro alunos.py
@@ -6,12 +6,19 @@ def mostrar_nome(nome):
         print(f"{i}. {aluno}")
     print("---------------------------------\n")
 while True:
-    nome_novo = input('Digite o nome: ').strip()
-    if not nome_novo:
-        print('Texto Inválido, tente novamente')
-        continue
-    lista_alunos.append(nome_novo)
-    mostrar_nome(nome_novo)
-    cont = input('Quer continuar? (s/n): ')
-    if cont.lower() == 'n':
-        break
+    print("=== MENU PRINCIPAL ===")
+    print("1- Cadastrar aluno")
+    print("2- Remover aluno")
+    print("3- Sair")
+    opcao = input("Escola a opção que você deseja: ").strip()
+    if opcao == '1':
+        while True
+            nome_novo = input('Digite o nome: ').strip()
+            if not nome_novo:
+                print('Texto Inválido, tente novamente')
+            continue
+        lista_alunos.append(nome_novo)
+        mostrar_nome(nome_novo)
+        cont = input('Quer continuar? (s/n): ')
+        if cont.lower() == 'n':
+            break
