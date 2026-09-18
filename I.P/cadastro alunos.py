1db68310db5663e70ea9e1f6a2e6e440393efc04
VS-ESCOLA <emanuel.romero@escola.pr.gov.br>
2026-09-18 11:39:21 -0300

    atualização

--- I.P/cadastro alunos.py
+++ I.P/cadastro alunos.py
@@ -3,10 +3,10 @@ lista_alunos = []
 def mostrar_nome(nome):
     print('Novo aluno cadastrado: ', nome)
     print(lista_alunos)
-while true:
+while True:
     nome_novo = input('Digite o nome: ')
-    lista_alunos.append()
+    lista_alunos.append(nome_novo)
     mostrar_nome(nome_novo)
-cont = print('Quer continuar? (s/n): ') 
-if cont.lower() = 'n':
-    break
+    cont = input('Quer continuar? (s/n): ')
+    if cont.lower() == 'n':
+        break
