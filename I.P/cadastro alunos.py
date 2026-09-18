51940b19ff8e46184b2e6e5fc7434c30cd4a05c6
VS-ESCOLA <emanuel.romero@escola.pr.gov.br>
2026-09-18 11:50:21 -0300

    lista melhor

--- I.P/cadastro alunos.py
+++ I.P/cadastro alunos.py
@@ -1,10 +1,12 @@
 lista_alunos = []
 
 def mostrar_nome(nome):
-    print('Novo aluno cadastrado: ', nome)
-    print(lista_alunos)
+    print("\n--- Lista de Alunos Cadastrados  ---")
+    for i, aluno in enumerate(lista_alunos, 1):
+        print(f"{i}. {aluno}")
+    print("---------------------------------\n")
 while True:
-    nome_novo = input('Digite o nome: ')
+    nome_novo = input('Digite o nome: ').strip()
     lista_alunos.append(nome_novo)
     mostrar_nome(nome_novo)
     cont = input('Quer continuar? (s/n): ')
